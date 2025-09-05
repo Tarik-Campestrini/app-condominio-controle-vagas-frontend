15028675d81e7f56aaec66035e6cbb599ae50ab8
TarikCampestrini <tarikrc@hotmail.com>
2025-09-05 14:33:10 -0300

    Deploy Vercel

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,16 +1,10 @@
+import React from "react";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Index from "../pages/Index";
-import { useState } from 'react';
+
 
 
 const RoutesApp = () => {
-  const [email, setEmail] = useState('');
-
-  const handleSubscribe = (e) => {
-    e.preventDefault();
-    alert(`Subscribed with email: ${email}`);
-  };
-
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900 text-center font-sans">
       <div className="p-8 max-w-lg mx-auto">

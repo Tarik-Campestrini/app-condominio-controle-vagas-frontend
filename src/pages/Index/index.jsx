1e6111b6c631ccaf9e6e6cba4a760652c04bd256
TarikCampestrini <tarikrc@hotmail.com>
2025-09-05 15:58:29 -0300

    ajuste na página inicial para exibir imagem de manutenção

--- src/pages/Index/index.jsx
+++ src/pages/Index/index.jsx
@@ -1,9 +1,10 @@
+
 const Index = () => {
   return (
         <div className="flex flex-col min-h-screen bg-gray-100 dark:bg-gray-900 transition-colors duration-500">
       
       <main className="flex-grow flex flex-col items-center justify-center">
-        <img src="src/assets/manutencao.png" alt="" />
+         <img src="src\assets\manutencao.png" alt="Página em manutenção" className="w-64 mx-auto" />
       </main>
     </div>
   );

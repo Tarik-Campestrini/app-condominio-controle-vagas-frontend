1fa29dfdf80047fee5c3b9a78be0e4722b6739f3
TarikCampestrini <tarikrc@hotmail.com>
2025-09-05 15:13:30 -0300

    Configurações Vercel e ajustes de importação

--- src/pages/Index/index.jsx
+++ src/pages/Index/index.jsx
@@ -3,8 +3,7 @@ const Index = () => {
         <div className="flex flex-col min-h-screen bg-gray-100 dark:bg-gray-900 transition-colors duration-500">
       
       <main className="flex-grow flex flex-col items-center justify-center">
-        <h1 className="text-4xl font-bold text-gray-800 dark:text-gray-200 mb-4">Welcome to the Index Page</h1>
-        <p className="text-lg text-gray-600 dark:text-gray-400">This is the main landing page of the application.</p>
+        <img src="src/assets/manutencao.png" alt="" />
       </main>
     </div>
   );
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,22 +1,14 @@
-import React from "react";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Index from "../pages/Index";
 
 
-
 const RoutesApp = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900 text-center font-sans">
-      <div className="p-8 max-w-lg mx-auto">
-        
-        <img
-          src="src/assets/manutencao.png"
-          alt="Under construction sign"
-          className="mx-auto my-8 w-64"
-        />        
-      </div>
-    </div>
+    <BrowserRouter>      
+      <Routes>        
+        <Route path="/" element={<Index />} />{" "}        
+      </Routes>     
+    </BrowserRouter>
   );
 };
-
 export default RoutesApp;

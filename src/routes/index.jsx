cc79c65c1fb668ed213c5267cd23b06e4df5e5fe
TarikCampestrini <tarikrc@hotmail.com>
2025-09-09 16:43:20 -0300

    Estruturação do projeto

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Index from "../pages/Index";
+import Login from "../pages/login";
 
 
 const RoutesApp = () => {
   return (
     <BrowserRouter>      
-      <Routes>        
-        <Route path="/" element={<Index />} />{" "}        
-      </Routes>     
+      <Routes>
+        <Route path="/" element={<Index />} />{" "}
+        <Route path="/login" element={<Login />} />{" "}
+      </Routes>
     </BrowserRouter>
   );
 };

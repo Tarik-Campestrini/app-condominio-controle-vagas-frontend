99b96736c94c95fe16df0d1dcc3d582c4450b3e7
TarikCampestrini <tarikrc@hotmail.com>
2025-09-19 16:43:09 -0300

    Ajuste api vercel

--- src/service/api.jsx
+++ src/service/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // API backend
+  baseURL: "https://app-condominio-controle-vagas-backe.vercel.app/api/", // API backend
 });
 
 export default api;
